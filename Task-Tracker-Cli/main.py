d769719ee13cbd3fff357a8c137b984cbcc79af0
Fadil <ibiyemifasasi@gmail.com>
2025-09-07 16:25:17 +0100

    completed add_tasks()

--- Task-Tracker-Cli/main.py
+++ Task-Tracker-Cli/main.py
@@ -5,39 +5,47 @@ from datetime import datetime
 filename = "tasks.json"
 
 def load_tasks():
-    try:
-        with open(filename, 'r') as file:
-            return json.load(file)
-    except FileNotFoundError:
-        with open(filename, 'w') as file:
-            json.dump([], file, indent=2)
-        
+    while True:
+        try:
+            with open(filename, 'r') as file:
+                tasks = json.load(file)
+                return tasks
+            
+        except FileNotFoundError:
+            save_tasks([])
+            continue
+
+def save_tasks(tasks):
+    with open(filename, 'w') as file:
+        json.dump(tasks, file, indent=2)
+
 def add_tasks(description, status="pending"):
     tasks = load_tasks()
     task_dict = {
-        'Id': len(tasks) + 1,
-        'description': description,
-        'status': status,
-        'createdAt': datetime.now().isoformat(),
-        'updatedAt': datetime.now().isoformat()
+        "Id": len(tasks) + 1,
+        "description": description,
+        "status": status,
+        "createdAt": datetime.now().strftime('%Y-%m-%d'),
+        "updatedAt": datetime.now().strftime('%Y-%m-%d')
     }
     tasks.append(task_dict)
-    with open(filename, 'w') as file:
-        json.dump(tasks, file, indent=2)
-    print(f"Task added: {task_dict['description']}")
-
+    save_tasks(tasks)
+    print("Task added")
 
 def main():
-    parser = argparse.ArgumentParser(description="A Task Tracking app")
-    subparsers = parser.add_subparsers(dest="command")
+    parser = argparse.ArgumentParser(description="A Task Tracking App")
+    subparsers = parser.add_subparsers(dest='command', help="Available commands")
 
-    add_parser = subparsers.add_parser("add", help="Adds a given task")
-    add_parser.add_argument('description', type=str, help='Task description')
+    # Add command
+    add_parser = subparsers.add_parser("add", description="Adds a given task")
+    add_parser.add_argument("description", type=str, help="Task description")
 
     args = parser.parse_args()
 
     if args.command == "add":
         add_tasks(args.description)
+    else:
+        parser.print_help()
 
 if __name__ == "__main__":
     main()
