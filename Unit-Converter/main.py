994c2b214f5abd976015486899108b2d040934e0
Fadil <ibiyemifasasi@gmail.com>
2025-11-16 22:46:30 +0100

    refactor: added support to use the convert module

--- Unit-Converter/main.py
+++ Unit-Converter/main.py
@@ -2,31 +2,25 @@ from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from pydantic import BaseModel
+from convert import *
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-recieved_arguments = []
-
-class Values(BaseModel):
-    entry: int
-    unit: str
-    new_unit: str
 
 @app.get("/", response_class=HTMLResponse)
 def read_form(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "data": None})
+    return templates.TemplateResponse("index.html", {"request": request, "result": None})
 
 @app.get("/weight", response_class=HTMLResponse)
 def return_form(request: Request):
-    return templates.TemplateResponse("weight.html", {"request": request, "data": None})
+    return templates.TemplateResponse("weight.html", {"request": request, "result": None})
 
 @app.get("/temperature", response_class=HTMLResponse)
 def return_form(request: Request):
-    return templates.TemplateResponse("temperature.html", {"request": request, "data": None})
+    return templates.TemplateResponse("temperature.html", {"request": request, "result": None})
 
 @app.post("/length", response_class=HTMLResponse)
 async def get_arguements(
@@ -35,8 +29,8 @@ async def get_arguements(
     unit: str = Form(...), 
     new_unit: str = Form(...)):
 
-    data = Values(entry=entry, unit=unit, new_unit=new_unit)
-    return templates.TemplateResponse("index.html", {"request": request, "data": data})
+    result = convert_temperature(entry, unit, new_unit)
+    return templates.TemplateResponse("index.html", {"request": request, "result": result})
 
 @app.post("/weight", response_class=HTMLResponse)
 async def get_arguements(
@@ -45,8 +39,8 @@ async def get_arguements(
     unit: str = Form(...), 
     new_unit: str = Form(...)):
 
-    data = Values(entry=entry, unit=unit, new_unit=new_unit)
-    return templates.TemplateResponse("weight.html", {"request": request, "data": data})
+    result = convert_temperature(entry, unit, new_unit)
+    return templates.TemplateResponse("weight.html", {"request": request, "result": result})
 
 @app.post("/temperature", response_class=HTMLResponse)
 async def get_arguements(
@@ -55,5 +49,5 @@ async def get_arguements(
     unit: str = Form(...), 
     new_unit: str = Form(...)):
 
-    data = Values(entry=entry, unit=unit, new_unit=new_unit)
-    return templates.TemplateResponse("temperature.html", {"request": request, "data": data})
+    result = convert_temperature(entry, unit, new_unit)
+    return templates.TemplateResponse("temperature.html", {"request": request, "result": result})
